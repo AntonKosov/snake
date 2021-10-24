cafe30f2b65a4b155393bce0aef2c59eb079b450
Anton Kosov <AntonRKosov@gmail.com>
2021-10-23 20:39:42 -0500

    build.sh was added

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 golangci-lint run || { echo 'lint failed' ; exit 1; }
+
+rm ./build/*
+env GOOS=windows GOARCH=amd64 go build -o ./build/snake.windows.amd64.exe .
+env GOOS=linux GOARCH=amd64 go build -o ./build/snake.linux.amd64 .
+env GOOS=darwin GOARCH=amd64 go build -o ./build/snake.darwin.amd64 .
