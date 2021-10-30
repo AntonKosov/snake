3c51b7b4acd56816d044be0bb85e5f2231ba479d
Anton Kosov <AntonRKosov@gmail.com>
2021-10-30 18:06:34 -0500

    Fixed build script

--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ golangci-lint run || { echo 'lint failed' ; exit 1; }
 rm ./build/*
 env GOOS=windows GOARCH=amd64 go build -o ./build/snake.windows.amd64.exe .
 env GOOS=linux GOARCH=amd64 go build -o ./build/snake.linux.amd64 .
+env GOOS=linux GOARCH=arm64 go build -o ./build/snake.linux.arm64 .
 env GOOS=darwin GOARCH=amd64 go build -o ./build/snake.darwin.amd64 .
